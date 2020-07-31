0b042ba7221a4578d507508de4180ac0023dc07f
Ashwini <ashwini.nikam7290@gmail.com>
2020-07-31 17:42:37 +0530

    Email Verification Link

--- app/src/main/java/com/example/nevihationapplication/login.java
+++ app/src/main/java/com/example/nevihationapplication/login.java
@@ -2,12 +2,18 @@ package com.example.nevihationapplication;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
 
@@ -19,11 +25,10 @@ public class login extends AppCompatActivity {
     String FirstN;
     String LastN;
 
-
-
-
     database db;
     singleDatabase singled;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,13 +43,18 @@ public class login extends AppCompatActivity {
         l=findViewById(R.id.login);
         r=findViewById(R.id.register);
 
-
+        firebaseAuth=FirebaseAuth.getInstance();
 
 
         r.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent in=new Intent(login.this,MainActivity.class);
+ //Original Code
+               // Intent in=new Intent(login.this,MainActivity.class);
+                //startActivity(in);
+ //Original Code
+
+                Intent in=new Intent(login.this,FirebaseRegistration.class);
                 startActivity(in);
             }
         });
@@ -53,8 +63,50 @@ public class login extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String eml=e.getEditText().getText().toString();
-                String psw=p.getEditText().getText().toString();
+                final String eml=e.getEditText().getText().toString();
+                final String psw=p.getEditText().getText().toString();
+
+
+
+                if(eml.equals("")||psw.equals("")==true)
+                {
+                    Toast.makeText(getApplicationContext(),"Please enter all details.",Toast.LENGTH_LONG).show();
+                }
+                else {
+
+
+                    firebaseAuth.signInWithEmailAndPassword(eml, psw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                if (firebaseAuth.getCurrentUser().isEmailVerified()) {
+
+                                    Cursor cr=singled.getFirstName(eml);
+                                    while (cr.moveToNext())
+                                    {
+                                        String FirstN=cr.getString(1);
+                                        String LastN=cr.getString(2);
+                                    }
+
+                                    Intent intent = new Intent(login.this, NevigationDraw.class);
+                                    intent.putExtra("First",FirstN);
+                                    intent.putExtra("Last",LastN);
+                                    intent.putExtra("Email",eml);
+                                    startActivity(intent);
+                                    Toast.makeText(getApplicationContext(),"Login successfully!!!",Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(login.this, "Please verify your email address", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Invalid email or password", Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    });
+
+
+                }
+
 
               //  Cursor cFL=db.getFirstName(eml);
                // while (cFL.moveToNext()) {
@@ -84,41 +136,41 @@ public class login extends AppCompatActivity {
              //   }
 
 
-                Cursor cFL=singled.getFirstName(eml);
-                while (cFL.moveToNext()) {
-                    FirstN = cFL.getString(1);
-                    LastN = cFL.getString(2);
-                }
 
-                if (eml.equals("") || psw.equals("")  == true) {
-                    Toast.makeText(getApplicationContext(), "Please enter all details.", Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
+//Single Database:
+               // Cursor cFL=singled.getFirstName(eml);
+            //    while (cFL.moveToNext()) {
+                 //   FirstN = cFL.getString(1);
+                  //  LastN = cFL.getString(2);
+               // }
 
-                    if(singled.isLogin(eml,psw))
-                    {
-                        Intent intent=new Intent(login.this,NevigationDraw.class);
-                        intent.putExtra("First",FirstN);
-                        intent.putExtra("Last",LastN);
-                        intent.putExtra("Email",eml);
-                        startActivity(intent);
-                        Toast.makeText(getApplicationContext(),"Login Successfully",Toast.LENGTH_LONG).show();
-                    }
-                    else
-                    {
-                        e.setError("Invalid email or password");
-                    }
-                }
+               // if (eml.equals("") || psw.equals("")  == true) {
+                 //   Toast.makeText(getApplicationContext(), "Please enter all details.", Toast.LENGTH_SHORT).show();
+              //  }
+               // else
+              //  {
+
+                //    if(singled.isLogin(eml,psw))
+                  //  {
+                      //  Intent intent=new Intent(login.this,NevigationDraw.class);
+                     //   intent.putExtra("First",FirstN);
+                     //   intent.putExtra("Last",LastN);
+                      //  intent.putExtra("Email",eml);
+                     //   startActivity(intent);
+                      //  Toast.makeText(getApplicationContext(),"Login Successfully",Toast.LENGTH_LONG).show();
+              //      }
+               //     else
+                 //   {
+                 //       e.setError("Invalid email or password");
+                //    }
+             //   }
+//Single Database
 
 
-
-            }
+           }
         });
-
 
     }
 
-
-
 }
+
---
+++ app/src/main/java/com/example/nevihationapplication/nevigationDrawer.java
@@ -0,0 +1,6 @@
+package com.example.nevihationapplication;
+
+import android.app.Activity;
+
+public class nevigationDrawer extends Activity {
+}
